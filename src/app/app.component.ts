6c6c6322432ab0bd57e0e271e8049cc85bd4f4ff
Leeniy <marleen@lh-net.de>
2021-04-28 17:48:29 +0200

    Aufsetzten des Grundgerüstes der Startseite ohne Funktionen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wt5';
+
+  update = false;
+
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      this.update = true;
+    });
+  }
 }
